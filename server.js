fbcf067e17d97744a5405ce213a520cdea42b383
ldevernay <boblehamster@yahoo.fr>
2017-08-10 12:23:46 +0200

    templates rendering

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const engines = require('consolidate');
 
 let app = express();
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/templates');
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('hello', {'name': 'Bob'});
 });
 
-app.listen(3000);
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+let server = app.listen(3000, () => {
+  let port = server.address().port;
+  console.log(`Listening to port ${port}.`);
+});
